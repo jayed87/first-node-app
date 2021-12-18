a4c2f42f52322764470b501f28f48a6477b8e650
JayedAkbarSumon <akbarjayed87@gmail.com>
2021-12-18 10:47:48 +0600

    users api and lookup by id added

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// const handler = (req, res)=>{
+//     res.send('Hello form node');
+// }
+// app.get('/',handler);
+const users = [
+    { id: 0, name: "jayed" },
+    { id: 1, name: "arif" },
+    { id: 2, name: "borhan" },
+    { id: 3, name: "rakesh" }
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello from the otherside and');
+})
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id];
+    res.send(user);
+})
+app.listen(port, () => {
+    console.log('listening to port', port);
+})
